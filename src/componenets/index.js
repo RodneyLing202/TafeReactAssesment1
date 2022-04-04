817f8fff0d589f8b66b07e6e2ba2af2f8326b2e7
Rodney Ling <moonashera@gmail.com>
2022-04-04 14:37:51 +1000

    updated to current

--- src/componenets/index.js
+++ src/componenets/index.js
@@ -1,3 +1,7 @@
 export * from "./button"
 export * from "./editTitle"
 export * from "./editBody"
+export * from "./noteTitle"
+export * from "./editTitle"
+export * from "./noteTitle"
+export * from "./noteBody"
