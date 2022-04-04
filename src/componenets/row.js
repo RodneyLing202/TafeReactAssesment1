0391a1fb094f8b2bf48bf5d0d314dc8941cfa2d8
Rodney Ling <moonashera@gmail.com>
2022-04-04 12:38:38 +1000

    implemented accordion

--- src/componenets/row.js
+++ src/componenets/row.js
@@ -3,31 +3,39 @@ import { NoteBody } from "./noteBody";
 import { Link } from "react-router-dom";
 import Draggable from "react-draggable";
 import Card from "react-bootstrap/Card";
+import Accordion from "react-bootstrap/Accordion";
+import Badge from "react-bootstrap/Badge";
 import { Button } from "./button";
 export const Row = ({ props }) => {
   return (
     <Draggable>
-      <Card style={{ width: "18rem", margin: "auto", marginTop:"3px"}}>
-        
-          <Card.Header>
+      <Accordion flush>
+        <Accordion.Header>
+          <Badge bg="primary">
             <NoteTitle title={props.Title} />
-          </Card.Header>
-          <Card.Body>
-          <Card.Text>
-            <NoteBody body={props.Body} />
-            <Card.Link>
-              <Link to={`editNote/${props.Id}`}>
-                <Button title="edit" buttonType="primary"></Button>
-              </Link>
-            </Card.Link>
-            <Card.Link>
-              <Link to={`deleteNote/${props.Id}`}>
-                <Button title="Delete" buttonType="secondary"></Button>
-              </Link>
-            </Card.Link>
-          </Card.Text>
-        </Card.Body>
-      </Card>
+          </Badge>
+        </Accordion.Header>
+        <Accordion.Body>
+          <NoteBody body={props.Body} />
+
+          <Card style={{ width: "18rem", margin: "auto", marginTop: "3px" }}>
+            <Card.Body>
+              <Card.Text>
+                <Card.Link>
+                  <Link to={`editNote/${props.Id}`}>
+                    <Button title="edit" buttonType="primary"></Button>
+                  </Link>
+                </Card.Link>
+                <Card.Link>
+                  <Link to={`deleteNote/${props.Id}`}>
+                    <Button title="Delete" buttonType="secondary"></Button>
+                  </Link>
+                </Card.Link>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Accordion.Body>
+      </Accordion>
     </Draggable>
   );
 };
