bcebe1492dc416ccaf25a469fe44817a18026d4d
Rodney Ling <moonashera@gmail.com>
2022-04-04 10:37:52 +1000

    moved data into card

--- src/componenets/row.js
+++ src/componenets/row.js
@@ -2,25 +2,31 @@ import { NoteTitle } from "./noteTitle";
 import { NoteBody } from "./noteBody";
 import { Link } from "react-router-dom";
 import Draggable from "react-draggable";
-
+import Card from "react-bootstrap/Card";
 export const Row = ({ props }) => {
-//   console.log(props);
+  //   console.log(props);
 
   return (
-      <Draggable>
+    <Draggable>
+      <Card style={{ width: '18rem' }}>
 
-      
-    <div>
-      row comp
-      <NoteTitle title={props.Title} />
-      <NoteBody body={props.Body} />
-      <Link to={`editNote/${props.Id}`}>
-        <button>edit</button>
-      </Link>
-      <Link to={`deleteNote/${props.Id}`}>
-        <button>Delete</button>
-      </Link>
-    </div>
+        <Card.Body>
+          <Card.Title>
+            <NoteTitle title={props.Title} />
+
+          </Card.Title>
+          <Card.Text>
+            <NoteBody body={props.Body} />
+            <Link to={`editNote/${props.Id}`}>
+              <button>edit</button>
+            </Link>
+            <Link to={`deleteNote/${props.Id}`}>
+              <button>Delete</button>
+            </Link>
+          </Card.Text>
+
+        </Card.Body>
+      </Card>
     </Draggable>
   );
 };
