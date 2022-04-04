d863c6dc2da025ac969cd014209ceb6faa3a7dda
Rodney Ling <moonashera@gmail.com>
2022-04-04 14:29:37 +1000

    added badge with my name

--- src/componenets/header.js
+++ src/componenets/header.js
@@ -2,6 +2,7 @@ import { Link, NavLink } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
+import Badge from "react-bootstrap/esm/Badge";
 export const Header = () => {
   let linkStyle = { textDecoration: "none", color: "white" };
 
@@ -9,8 +10,10 @@ export const Header = () => {
     <Navbar expand="lg" bg="dark" variant="dark">
       <Container>
         <Navbar.Brand>
+
           <NavLink style={linkStyle} to="/">
-            Notes
+            <Badge bg="light" text="dark">Rodney Ling: Notes</Badge>
+            
           </NavLink>
         </Navbar.Brand>
         <Nav>
