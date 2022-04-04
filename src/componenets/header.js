5d987c7d2070061d965354c238b5385ca408a024
Rodney Ling <moonashera@gmail.com>
2022-04-04 13:41:32 +1000

    added styling to header

--- src/componenets/header.js
+++ src/componenets/header.js
@@ -1,30 +1,43 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 export const Header = () => {
+  let linkStyle = { textDecoration: "none", color: "white" };
+
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar expand="lg" bg="dark" variant="dark">
       <Container>
         <Navbar.Brand>
-          <Link to="/">Notes</Link>
+          <NavLink style={linkStyle} to="/">
+            Notes
+          </NavLink>
         </Navbar.Brand>
+        <Nav>
+          <Nav.Link>
+            <NavLink style={linkStyle} to="/">
+              Home
+            </NavLink>
+          </Nav.Link>
 
-        <Nav.Link>
-          <Link to="/">Home</Link>
-        </Nav.Link>
+          <Nav.Link>
+            <NavLink style={linkStyle} to="/addNote">
+              Add A New Note
+            </NavLink>
+          </Nav.Link>
 
-        <Nav.Link>
-          <Link to="/addNote">Add A New Note</Link>
-        </Nav.Link>
+          <Nav.Link>
+            <NavLink style={linkStyle} to="/editNote">
+              edit an existing Note
+            </NavLink>
+          </Nav.Link>
 
-        <Nav.Link>
-          <Link to="/editNote">edit an existing Note</Link>
-        </Nav.Link>
-
-        <Nav.Link>
-          <Link to="/clearAll">Clear All Notes</Link>
-        </Nav.Link>
+          <Nav.Link>
+            <NavLink style={linkStyle} to="/clearAll">
+              Clear All Notes
+            </NavLink>
+          </Nav.Link>
+        </Nav>
 
         {/* <Link to="/addNote">Add A New Note</Link>
                 <span> </span>
