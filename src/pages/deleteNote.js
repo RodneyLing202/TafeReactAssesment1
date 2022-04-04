8d1410cc167442ec0c3067ed1722c2a0f51ea082
Rodney Ling <moonashera@gmail.com>
2022-04-04 14:38:24 +1000

    updated imports

--- src/pages/deleteNote.js
+++ src/pages/deleteNote.js
@@ -1,9 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { Button } from "../componenets";
-import { NoteBody } from "../componenets/noteBody";
-import { NoteTitle } from "../componenets/noteTitle";
-import { Row } from "../componenets/row";
+import { Button, NoteBody, NoteTitle} from "../componenets";
 import Storage from "../services/storage";
 
 export const DeleteNote = () => {
@@ -14,7 +11,6 @@ export const DeleteNote = () => {
   useEffect(() => {
     let Notes = Storage.getItem("Notes");
     let tempNote = Notes.filter((element) => element.Id == Id);
-    // console.log("tempNote", tempNote);
     setNote(tempNote[0]);
   }, []);
 
@@ -22,14 +18,12 @@ export const DeleteNote = () => {
     let Notes = Storage.getItem("Notes");
     let tempNotes = Notes.filter((element) => element.Id != Id);
     Storage.setItem("Notes", tempNotes);
-    // console.log("notesDelete", tempNotes);
     navigate("/");
   };
 
   return (
     <div>
       are you sure you want to delete Note {Id}
-      {/* <Row props={Note} /> */}
       <NoteTitle title={Note.Title}/>
       <NoteBody body = {Note.Body}/>
       <Button title={"Delete This Note"} onClick={deleteNote} />
