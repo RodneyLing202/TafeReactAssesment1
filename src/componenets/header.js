16465a3fc70d5296bcc2e02299b631b328a40662
Rodney Ling <moonashera@gmail.com>
2022-04-04 10:47:39 +1000

    added bootstrap to navbar

--- src/componenets/header.js
+++ src/componenets/header.js
@@ -1,20 +1,37 @@
 import { Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+export const Header = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand>
+          <Link to="/">Notes</Link>
+        </Navbar.Brand>
 
+        <Nav.Link>
+          <Link to="/">Home</Link>
+        </Nav.Link>
 
-export const Header = () =>{
-    return(
-    <div className="links">
-            React Router
+        <Nav.Link>
+          <Link to="/addNote">Add A New Note</Link>
+        </Nav.Link>
 
-            <Link to="/">home</Link>
-            <span> </span>
-            <Link to="/addNote">Add A New Note</Link>
-            <span> </span>
-            <Link to="/editNote">edit an existing Note</Link>
-            <span> </span>
-            <Link to="/clearAll">Clear All Notes</Link> 
+        <Nav.Link>
+          <Link to="/editNote">edit an existing Note</Link>
+        </Nav.Link>
 
-        </div>
-    )
-}
-            
+        <Nav.Link>
+          <Link to="/clearAll">Clear All Notes</Link>
+        </Nav.Link>
+
+        {/* <Link to="/addNote">Add A New Note</Link>
+                <span> </span>
+                <Link to="/editNote">edit an existing Note</Link>
+                <span> </span>
+                <Link to="/clearAll">Clear All Notes</Link> */}
+      </Container>
+    </Navbar>
+  );
+};
