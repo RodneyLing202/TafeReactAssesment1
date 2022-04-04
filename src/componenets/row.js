47ffcb5615c578b714921415b09b3e1faf0a6490
Rodney Ling <moonashera@gmail.com>
2022-04-04 11:16:52 +1000

    imported button

--- src/componenets/row.js
+++ src/componenets/row.js
@@ -3,28 +3,26 @@ import { NoteBody } from "./noteBody";
 import { Link } from "react-router-dom";
 import Draggable from "react-draggable";
 import Card from "react-bootstrap/Card";
+import { Button } from "./button";
 export const Row = ({ props }) => {
   //   console.log(props);
-
+  //
   return (
     <Draggable>
-      <Card style={{ width: '18rem' }}>
-
+      <Card style={{ width: "18rem" }}>
         <Card.Body>
           <Card.Title>
             <NoteTitle title={props.Title} />
-
           </Card.Title>
           <Card.Text>
             <NoteBody body={props.Body} />
             <Link to={`editNote/${props.Id}`}>
-              <button>edit</button>
+              <Button title="edit" buttonType="primary"></Button>
             </Link>
             <Link to={`deleteNote/${props.Id}`}>
-              <button>Delete</button>
+              <Button title="Delete" buttonType="secondary"></Button>
             </Link>
           </Card.Text>
-
         </Card.Body>
       </Card>
     </Draggable>
