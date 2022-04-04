0baf6f693e911c728aa58445379ff67e2dc5793c
Rodney Ling <moonashera@gmail.com>
2022-04-04 10:37:25 +1000

    import bootstrap styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Router } from './componenets/router';
 import { Header } from './componenets/header';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
