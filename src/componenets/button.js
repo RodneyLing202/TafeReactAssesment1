cdc616ba0f827ef8480ede9fc9442fc9c9aa7eb8
Rodney Ling <moonashera@gmail.com>
2022-04-04 11:17:08 +1000

    utlises bootstrap button

--- src/componenets/button.js
+++ src/componenets/button.js
@@ -1,7 +1,18 @@
 import React from "react";
 // import classNames from './button.module.css';
-
+import {default as BootstrapButton} from "react-bootstrap/Button";
 export function Button(props) {
+  let buttonType = ""
+  
+  if (props.buttonType == null) 
+    buttonType = "primary"
+  
+  else
+    buttonType = props.buttonType
+
   const { title, onClick } = props; // object destructuring
-  return <button onClick={onClick}>{title}</button>;
+  // return <button onClick={onClick}>{title}</button>;
+  return(
+    <BootstrapButton variant={buttonType} onClick={onClick}>{title}</BootstrapButton>
+  )
 }
